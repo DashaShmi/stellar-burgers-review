914633f0dddd3259d459a8be31448348b884ca2a
Daria Dmitrienko <ishmizh@gmail.com>
2025-02-05 18:58:02 +0400

    тесты модалки

--- cypress/e2e/contructor/constructor.cy.ts
+++ cypress/e2e/contructor/constructor.cy.ts
@@ -16,14 +16,14 @@ describe('проверяем добавление ингредиента из с
     cy.visit('http://localhost:4000/');
 
     const bulkaId = '643d69a5c3f7b9001cfa093c';// краторная булка
-    // находим в DOM дереве кнопку с атрибутом data-cy=1
+    // находим в DOM дереве кнопку с атрибутом data-cy=
     const li = cy.get(`[data-cy=ingridient-${bulkaId}]`)
     const addButton = li.find('button');
     // кликаем по ней
     addButton.click();
 
-    const bunContainernTop = cy.get(`[data-cy=bun-top]`)
-    const bunContainernBottom = cy.get(`[data-cy=bun-bottom]`)
+    const bunContainernTop = cy.get(`[data-cy=bun-top]`);
+    const bunContainernBottom = cy.get(`[data-cy=bun-bottom]`);
 
     // проверяем что в div попал текст вверхней булки
     bunContainernTop.contains('Краторная булка N-200i');
@@ -31,8 +31,22 @@ describe('проверяем добавление ингредиента из с
 
     // проверяем что в div попал текст нижней булки
     bunContainernBottom.contains('(низ)')
-  })
+  });
 
+  it('добавление начинок', () => {
+    cy.visit('http://localhost:4000/');
+
+    // находим в DOM дереве кнопку с атрибутом data-cy=
+    const ingidient1 = cy.get(`[data-cy=ingridient-643d69a5c3f7b9001cfa0941]`);// Биокотлета из марсианской Магнолии
+    const ingidient2 = cy.get(`[data-cy=ingridient-643d69a5c3f7b9001cfa093e]`);// Филе Люминесцентного тетраодонтимформа
+
+    ingidient1.find('button').click();
+    ingidient2.find('button').click();
+
+    cy.get(`[data-cy=no-bun]`).should("have.length", 2);
+    cy.get(`[data-cy=no-bun]`).eq(0).contains("Биокотлета из марсианской Магнолии");
+    cy.get(`[data-cy=no-bun]`).eq(1).contains("Филе Люминесцентного тетраодонтимформа");
+  });
 
 })
 
