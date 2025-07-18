d54f989c189e9afd009b0cfe48b71ed74519d729
Daria Dmitrienko <ishmizh@gmail.com>
2025-07-18 18:23:28 +0400

    добавила HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import App from './components/app/app';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
